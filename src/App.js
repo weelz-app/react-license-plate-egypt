bd6887da0fbdeedbaa478e26010edc5b577e5372
Aser Samak <asamak@Asers-MacBook-Pro.local>
2021-04-10 14:51:32 +0200

    Working version of Egyptian license plate with width and type props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import LicensePlate from './components/LicensePlate'
+import { VehicleType } from './components/LicensePlate'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+  return <LicensePlate number={'سسس ٨٨٨٨'} />
 }
 
-export default App;
+export default App
---
+++ src/components/LicensePlate.js
@@ -0,0 +1,97 @@
+import React from 'react'
+
+export const VehicleType = {
+  PRIVATE: '#0099FF',
+  TAXI: '#FFA343',
+  TRUCK: '#FF5733',
+  BUSS: '#C4C4C4',
+  TOURISIM: '#CBB58B',
+  DIPLOMATIC: '#60AC54',
+  CUSTOMS: '#F7CC5E'
+}
+
+function LicensePlate ({
+  vehicle = VehicleType.PRIVATE,
+  width = 200,
+  number = ''
+}) {
+  const mainDiv = {
+    width: `${width}px`
+  }
+  const topDiv = {
+    boxSizing: 'border-box',
+    borderTopLeftRadius: `${width / 30}px`,
+    borderTopRightRadius: `${width / 30}px`,
+    border: `${width / 100}px solid`,
+    borderBottom: `${width / 200}px solid`,
+    width: '100%',
+    paddingTop: '2%',
+    paddingBottom: '15%',
+    paddingLeft: '5%',
+    paddingRight: '5%',
+    background: `${vehicle}`,
+    position: 'relative'
+  }
+  const bottomDiv = {
+    boxSizing: 'border-box',
+    borderBottomLeftRadius: `${width / 30}px`,
+    borderBottomRightRadius: `${width / 30}px`,
+    border: `${width / 100}px solid`,
+    borderTop: `${width / 200}px solid`,
+    width: '100%',
+    paddingTop: '5%',
+    paddingBottom: '25%',
+    paddingLeft: '1%',
+    paddingRight: '1%',
+    position: 'relative'
+  }
+
+  const countryEng = {
+    fontWeight: 'bold',
+    float: 'left',
+    fontSize: width / 10
+  }
+
+  const countryAr = {
+    fontWeight: 'bold',
+    float: 'right',
+    fontSize: width / 11
+  }
+
+  const lettersAr = {
+    float: 'right',
+    fontSize: width / 7
+  }
+
+  const digitsAr = {
+    fontWeight: 'bold',
+    float: 'left',
+    fontSize: width / 7
+  }
+
+  return (
+    <div style={mainDiv}>
+      <div style={topDiv}>
+        <div style={countryEng}>EGYPT</div>
+        <div style={countryAr}>مصر</div>
+      </div>
+      <div style={bottomDiv}>
+        <div style={digitsAr}>{digits(number)}</div>
+        <div style={lettersAr}>{letters(number)}</div>
+      </div>
+    </div>
+  )
+}
+
+// ِExtract digits
+function digits (text) {
+  return text.replace(/\s/g, '').match('[\u0660-\u0669]+')
+}
+
+// ِExtract individual characters and join them as seperated letters
+function letters (text) {
+  let chars = text.replace(/\s/g, '').match(/[^\u0660-\u0669]/g)
+  return chars ? chars.join('\u200C') : ''
+}
+
+export default LicensePlate
